9de75a8a024f583a6ce17a73318c25261081b4b5
DeepaManasa <147066564+DeepaManasa@users.noreply.github.com>
2024-11-08 07:48:15 -0500

    Update Externaltablequeries.sql

--- Externaltablequeries.sql
+++ Externaltablequeries.sql
@@ -1,3 +1,46 @@
+IF NOT EXISTS (SELECT * FROM sys.external_file_formats WHERE name = 'SynapseDelimitedTextFormat') 
+	CREATE EXTERNAL FILE FORMAT [SynapseDelimitedTextFormat] 
+	WITH ( FORMAT_TYPE = DELIMITEDTEXT ,
+	       FORMAT_OPTIONS (
+			 FIELD_TERMINATOR = ',',
+			 FIRST_ROW = 2,
+			 USE_TYPE_DEFAULT = FALSE
+			))
+GO
+
+
+IF NOT EXISTS (SELECT * FROM sys.external_data_sources WHERE name = 'raw-data_storagesynapse97_dfs_core_windows_net') 
+	CREATE EXTERNAL DATA SOURCE [raw-data_storagesynapse97_dfs_core_windows_net] 
+	WITH (
+		LOCATION = 'abfss://raw-data@storagesynapse97.dfs.core.windows.net' 
+	)
+GO
+
+
+CREATE EXTERNAL TABLE customer (
+	[CustomerID] NVARCHAR(1000),
+	[NameStyle] NVARCHAR(1000),
+	[Title] NVARCHAR(1000),
+	[FirstName] nvarchar(1000),
+	[MiddleName] nvarchar(1000),
+	[LastName] nvarchar(1000),
+	[Suffix] nvarchar(1000),
+    [CompanyName] nvarchar(1000),
+    [SalesPerson] nvarchar(1000),
+    [EmailAddress] nvarchar(1000),
+    [Phone] nvarchar(1000),
+    [PasswordHash] nvarchar(1000),
+    [PasswordSalt] nvarchar(1000),
+    [rowguid] nvarchar(1000),
+    [ModifiedDate] nvarchar(1000)
+	)
+	WITH (
+	LOCATION = '/SalesLT/Customer/',
+	DATA_SOURCE = [raw-data_storagesynapse97_dfs_core_windows_net],
+	FILE_FORMAT = [SynapseDelimitedTextFormat]
+	)
+GO
+
 
 SELECT TOP 10 * FROM customer;
 
